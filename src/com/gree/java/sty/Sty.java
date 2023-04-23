b87017d636025ed5c9006f9ab700ac5cd2df75f7
Hyeon <zkti12345@naver.com>
2023-04-23 20:27:46 +0900

    home sty

---
+++ src/com/gree/java/sty/Sty.java
@@ -0,0 +1,15 @@
+package com.gree.java.sty;
+
+public class Sty {
+    public static void show(){
+        System.out.println(" i am show method");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("before");
+        show();
+        System.out.println("good");
+    }
+
+}
