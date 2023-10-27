6618541e2bb76b410d9473c15cea1f38d9bfd15c
zkti1 <zkti12345@naver.com>
2023-10-27 16:18:42 +0900

    Collections

---
+++ src/com/green/javaHome/sty/study_one/collections_framework/CollectionsEx.java
@@ -0,0 +1,15 @@
+package com.green.javaHome.sty.study_one.collections_framework;
+
+public class CollectionsEx {
+    public static void main(String[] args) {
+        /*
+        -컬렉션을 위한 메서드(static)를 제공
+        1.컬렉션 채우기, 복사 ,정렬, 검색 - fill(), copy(), sort(),binarySearch()등
+        2.컬렉션의 동기화 - synchronizedXXX()
+        3.변경불가(readOnly) 컬렉션 만들기 - unmodufiableXXX()
+        4.싱글톤 컬렉션 만들기 <= 객체 1개만 저장 singletonXXX()
+        5.한 종류의 객체만 저장하는 걸렉션 만들기 checkedXXX() 한가지 타입만 저장가능
+
+         */
+    }
+}
