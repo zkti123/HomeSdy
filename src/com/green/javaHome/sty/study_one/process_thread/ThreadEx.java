6b8bc59cc4be4abe170c0a1f6085ca8f206b2743
zkti1 <zkti12345@naver.com>
2023-10-30 15:55:26 +0900

    Process_Thread 10/30

---
+++ src/com/green/javaHome/sty/study_one/process_thread/ThreadEx.java
@@ -0,0 +1,18 @@
+package com.green.javaHome.sty.study_one.process_thread;
+
+public class ThreadEx {
+    public static void main(String[] args) {
+        /*
+             1.  Thread 의 상속
+               class myThread extends Thread{
+               public void run(){ 작업 내용}   <==Thread 클래스의 run()을 오버 라이딩
+               }
+
+             2. Runnable 인터페이스를 구현
+                class myThread implements Runnable{
+                  public void run(){ 작업 내용}   <==Runnable 인터페이스의 run()을 구현
+               }
+
+         */
+    }
+}
---
+++ src/com/green/javaHome/sty/study_one/process_thread/ThreadEx1.java
@@ -0,0 +1,34 @@
+package com.green.javaHome.sty.study_one.process_thread;
+
+public class ThreadEx1 {
+    public static void main(String[] args) {
+        TreadEx1_1 t1 = new TreadEx1_1();
+
+        Runnable r = new TreadEx1_2();
+
+        Thread t2 = new Thread(r);
+
+        t1.start();
+        t2.start();
+
+
+
+    }
+}
+class TreadEx1_1 extends Thread{
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(getName()); // 조상인 Thread의 getName()을 호출
+        }
+    }
+}
+
+class TreadEx1_2 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            // Thread.currentThread() <== 현재 실행중인 Thread를 반환한다.
+            System.out.println(Thread.currentThread().getName());
+        }
+
+    }
+}
