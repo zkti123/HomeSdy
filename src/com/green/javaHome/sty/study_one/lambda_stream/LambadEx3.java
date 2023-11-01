499a1a25d9cb58c07fae79fe87ab810c42f57d8a
zkti1 <zkti12345@naver.com>
2023-11-01 16:15:48 +0900

    Lambda 11/1

---
+++ src/com/green/javaHome/sty/study_one/lambda_stream/LambadEx3.java
@@ -0,0 +1,33 @@
+package com.green.javaHome.sty.study_one.lambda_stream;
+
+@FunctionalInterface
+interface MyFunction3 {
+    void myMethod();
+}
+
+class Outer {
+    int val = 10;
+
+    class Inner {
+        int val = 20;
+
+        void method(int i) { // <<== void method(final int i){}
+            int val = 30;  // <<  final int val = 30;
+          //  i = 10; 에러 상수값은 변경불가
+            MyFunction3 f = () ->{
+                System.out.println("         i : " + i);
+                System.out.println("       val : " + val);
+                System.out.println("  this.val : " + ++this.val);
+                System.out.println("Outer.this.val : " +  ++Outer.this.val);
+            };
+            f.myMethod();
+        }
+    }
+}
+public class LambadEx3 {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+        inner.method(100);
+    }
+}
