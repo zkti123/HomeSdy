ae9a8964aef46c7b123f1dcb1d9a4a104c3e9755
zkti1 <zkti12345@naver.com>
2023-11-14 11:40:38 +0900

    array 공부 11/14

---
+++ src/com/green/javaHome/sty/study_one/array/Array_Ex06.java
@@ -0,0 +1,24 @@
+package com.green.javaHome.sty.study_one.array;
+
+public class Array_Ex06 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = i;
+            System.out.print(numArr[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < 100; i++) {
+            int n = (int) (Math.random() * 10);
+            int tmp = numArr[0];
+            numArr[0] = numArr[n];
+            numArr[n] = tmp;
+        }
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.print(numArr[i]);
+        }
+
+    }
+}
