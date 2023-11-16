cd0e04f7ed671df92dab65a69a7eee9002412c72
zkti1 <zkti12345@naver.com>
2023-11-16 11:44:48 +0900

    객체지향 공부 11/16

---
+++ src/com/green/javaHome/sty/study_one/programming_language/PrimitiveParamEx2.java
@@ -0,0 +1,45 @@
+package com.green.javaHome.sty.study_one.programming_language;
+
+public class PrimitiveParamEx2 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{3, 2, 1, 6, 5, 4};
+
+        printArr(arr); //배열의 모든 요소를 출력
+        sortArr(arr); // 배열을 정령
+        printArr(arr);// 정렬된 배열을 출력
+        System.out.println("sum = " + sumArr(arr)); // 배열의 총합을 출력
+
+    }
+
+    static void printArr(int[] arr) {
+        System.out.print("[");
+        for (int i : arr) {
+            System.out.print(i + ",");
+
+        }
+        System.out.println("]");
+    }
+
+    static int sumArr(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+        }
+        return sum;
+    }
+
+    static void sortArr(int[] arr) {  //배열의 오름차순으로 정령
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j] > arr[j + i]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                }
+
+            }
+        }
+    }
+}
