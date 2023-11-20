22e5de5c406ed45b5482433537208e1a8c042af7
zkti1 <zkti12345@naver.com>
2023-11-20 13:04:25 +0900

    객체지향프로그래밍 공부 11/20

---
+++ src/com/green/javaHome/sty/study_one/programming_language/construtor/CarEx1.java
@@ -0,0 +1,30 @@
+package com.green.javaHome.sty.study_one.programming_language.construtor;
+
+public class CarEx1 {
+    public static void main(String[] args) {
+        Car1 c1 = new Car1();
+        Car1 c2 = new Car1("blue");
+
+        System.out.println("c1의 color : "+c1.color+",gearType : "+c1.gearType+",door = "+ c1.door);
+        System.out.println("c2의 color : "+c2.color+",gearType : "+c2.gearType+",door = "+ c2.door);
+    }
+}
+class Car1{
+    String color;
+    String gearType;
+    int door;
+
+    Car1() {
+        this("white", "auto", 5);
+    }
+
+    Car1(String color) {
+        this(color, "auto", 4);
+    }
+
+    public Car1(String color, String gearType, int door) {
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
+    }
+}
