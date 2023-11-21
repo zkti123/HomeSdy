c93a693467b61271e31b798d4c091b8f8c6e8955
zkti1 <zkti12345@naver.com>
2023-11-21 11:26:02 +0900

    객체지향프로그래밍 공부 11/21

---
+++ src/com/green/javaHome/sty/study_one/programming_language2/StaticImportEx1.java
@@ -0,0 +1,13 @@
+package com.green.javaHome.sty.study_one.programming_language2;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.random;
+import static java.lang.System.out;
+
+public class StaticImportEx1 {
+    public static void main(String[] args) {
+//        out.println(Math.random());
+        out.println(random());
+        out.println("Math.PI : "+PI);
+    }
+}
