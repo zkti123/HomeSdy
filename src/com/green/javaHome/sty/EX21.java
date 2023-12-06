63e6ad1808e10f64a2b02c41df9a27376798e1dc
zkti1 <zkti12345@naver.com>
2023-12-06 14:19:01 +0900

    입출력 공부 12/06

--- src/com/green/javaHome/sty/EX21.java
+++ src/com/green/javaHome/sty/EX21.java
@@ -2,9 +2,12 @@ package com.green.javaHome.sty;
 
 public class EX21 {
     public static void main(String[] args) {
-        for (int i = 9; i >= 2; i--) {
+        int num = 9;
+        int dan = 1;
+        int nw = 2;
+        for (int i = num; i >= nw; i--) {
             System.out.println(i + "단");
-            for (int j = 1; j <= 9; j++) {
+            for (int j = dan; j <= num; j++) {
                 System.out.println(i + " x " + j + " = " + (i * j));
             }
             System.out.println(); // 각 단마다 빈 줄 추가
