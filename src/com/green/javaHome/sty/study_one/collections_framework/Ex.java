59d4cd9a52aae2a0fe737d0949cda869ff04ed6e
zkti1 <zkti12345@naver.com>
2023-12-07 10:52:38 +0900

    공부 12/07

---
+++ src/com/green/javaHome/sty/study_one/collections_framework/Ex.java
@@ -0,0 +1,28 @@
+package com.green.javaHome.sty.study_one.collections_framework;
+
+public class Ex {
+    public static void main(String[] args) {
+        /*/
+        List : 순서가 있는 데이터의 집합. 데이터의 중복을 허용한다.
+        구현클래스 : ArrayList, LinkedList, Stack, Vector 등
+
+        Set  : 순서를 유지 하지않는 데이터의 집합. 데이터의 중복을 허용하지 않는다.
+                예) 양의 정수 집합,소수의 집합
+        구현클래스 : HashSet,TreeSet 등
+
+        Map  : 키와 밸류의 쌍으로 이루어진 데이터의 집합
+                순서는 유지되지 않으며, 키는 중복을 허용하지 않고,값은 중복을 허용한다.
+                예) 우편번호, 지역번호
+        구현클래스 : HashMap, TreeMap, HashTable, Properties 등
+
+
+        키란,데이터 집합중에서 어떤 값(value)을 찾는데 열쇠(key)가 된다는 의미에서 붙여진 이름이다. 그래서 키는 중복허용이 안된다.
+
+        List 인터페이스 : 중복을 허용하면서 저장순서가 유지되는 컬렉션을 구현하는데 사용된다.
+        Set  인터페이스 : 중복을 허용하지 않고 저장순서가 유지 되지 않는 컬렉션클래스를 구현하는데 사용된다.
+        Map  인터페이스 : 키와 값을 하나의 쌍으로 묶어서 저장하는 컬렉션 클래스를 구현하는 데 사용된다. 키는 중복X 값은 중복 허용.
+
+        ArrayList : List인터페이스를 구현하며 Object배열을 이용해서 데이터를 순차적으로 저장한다.
+         */
+    }
+}
