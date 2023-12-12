b7f117bdbfd0894d140be323b6c44dba1f30e691
zkti1 <zkti12345@naver.com>
2023-12-12 10:37:13 +0900

    공부 12/12

---
+++ src/com/green/javaHome/sty/study_one/variable/PrintfEx1.java
@@ -0,0 +1,17 @@
+package com.green.javaHome.sty.study_one.variable;
+
+public class PrintfEx1 {
+    public static void main(String[] agrs) {
+        byte b = 1;
+        short s = 2;
+        char c ='A';
+        int finger = 10;
+
+        System.out.printf("b=%d\n", b);
+        System.out.printf("s=%d%n", s);
+        System.out.printf("c=%c, %d %n", c, (int)c);
+        System.out.printf("finger=[%5d] \n", finger);//  \ => 이스케이프 문자 개행문자
+        System.out.printf("finger=[%-5d] \n", finger);
+        System.out.printf("finger=[%05d] \n", finger);
+    }
+}
