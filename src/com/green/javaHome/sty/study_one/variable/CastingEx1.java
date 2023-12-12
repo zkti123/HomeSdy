6cf63eb7cf2e921f23f7a3629346cee6d6b9b2ff
zkti1 <zkti12345@naver.com>
2023-12-12 09:59:21 +0900

    공부 12/12

---
+++ src/com/green/javaHome/sty/study_one/variable/CastingEx1.java
@@ -0,0 +1,13 @@
+package com.green.javaHome.sty.study_one.variable;
+
+public class CastingEx1
+{
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 3;
+        double r = (double)n1 / n2;
+
+        System.out.println(r);
+        //%.3(소수점자리)f
+    }
+}
