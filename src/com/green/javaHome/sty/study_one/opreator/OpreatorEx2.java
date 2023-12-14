09f6c8a75febdde1202cc1d8e67061c94686a00f
zkti1 <zkti12345@naver.com>
2023-12-15 07:25:24 +0900

    공부 12/15

---
+++ src/com/green/javaHome/sty/study_one/opreator/OpreatorEx2.java
@@ -0,0 +1,16 @@
+package com.green.javaHome.sty.study_one.opreator;
+
+public class OpreatorEx2 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        int result = num % 2;
+        System.out.printf("%d %%2 = %d\n", num, result);
+
+        num = 9;
+        result = num % 2;
+        System.out.printf("%d %%2 = %d\n", num, result);
+
+
+    }
+}
---
+++ src/com/green/javaHome/sty/study_one/opreator/OpreatorEx3.java
@@ -0,0 +1,19 @@
+package com.green.javaHome.sty.study_one.opreator;
+
+public class OpreatorEx3 {
+    public static void main(String[] args) {
+
+
+        int r = -3-5;
+        //int r = -3 - +5;
+        System.out.println(r);
+
+        int r2 = 5 - 3;
+        System.out.println(r2);
+
+
+        int r3 = -3+5;
+        System.out.println(r3);
+
+    }
+}
