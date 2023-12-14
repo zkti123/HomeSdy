05dc18526d68e1891f6ed80a813421f427b8cb78
zkti1 <zkti12345@naver.com>
2023-12-14 13:05:33 +0900

    공부 12/14

---
+++ src/com/green/javaHome/sty/study_one/variable/VariableString.java
@@ -0,0 +1,18 @@
+package com.green.javaHome.sty.study_one.variable;
+
+public class VariableString {
+    public static void main(String[] args) {
+        String s1 = "Hello";  //주소값 복사 /변수의 대문자로 시작하면 주소값이다 String = 문자열
+        String s2 = "World";
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        String s3 = s1 + s2; // +(문자열 합 칠 때)
+        //String s3 = "HelloWorld"
+        System.out.println(s3);
+
+        String s4 = s1 + " " + s2;
+        System.out.println(s4);
+    }
+}
---
+++ src/com/green/javaHome/sty/study_one/variable/VariableString2.java
@@ -0,0 +1,18 @@
+package com.green.javaHome.sty.study_one.variable;
+
+public class VariableString2 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "hello";
+
+        System.out.println(s1 == s2);
+
+        String s3 = new String("hello");
+        String s4 = new String("hello");
+
+        System.out.println("s3 : " + s3);
+        System.out.println("s4 : " + s4);
+        System.out.println("s3==s4: "+ (s3 == s4));
+        System.out.println("s3.equals(s4): "+ s3.equals(s4));
+    }
+}
