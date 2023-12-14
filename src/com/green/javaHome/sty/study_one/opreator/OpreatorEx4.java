9ed90c4e5ce81995473bc71ee46ce7c20f4cf031
zkti1 <zkti12345@naver.com>
2023-12-15 07:26:42 +0900

    공부 12/15

---
+++ src/com/green/javaHome/sty/study_one/opreator/OpreatorEx4.java
@@ -0,0 +1,15 @@
+package com.green.javaHome.sty.study_one.opreator;
+
+public class OpreatorEx4 {
+    public static void main(String[] args) {
+
+        int n1, n2, val = 10;
+
+        n1 = n2 = val;  // val=n2=n1 순서로
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(val);
+
+    }
+}
