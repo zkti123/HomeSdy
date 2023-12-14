408ff218f64ff4b9cbde0c89f7c265358242f359
zkti1 <zkti12345@naver.com>
2023-12-14 13:08:03 +0900

    공부 12/14

---
+++ src/com/green/javaHome/sty/study_one/variable/VariableBoolean.java
@@ -0,0 +1,14 @@
+package com.green.javaHome.sty.study_one.variable;
+
+public class VariableBoolean {
+    public static void main(String[] args) {
+        boolean b1 = true, b2 =false;
+        System.out.println(b1);
+        System.out.println(b2);
+
+        b2 = 10 > 2;
+        System.out.println(b2);
+        b2 = 10<10;
+        System.out.println(b2);
+    }
+}
---
+++ src/com/green/javaHome/sty/study_one/variable/VariableChar.java
@@ -0,0 +1,15 @@
+package com.green.javaHome.sty.study_one.variable;
+
+public class VariableChar {
+    public static void main(String[] args) {
+        //변수 선언과 동시에 초기화 가능
+        char c1 = 'A';//문자, 문자열X,홑따옴표 사용.
+        System.out.println(c1);
+
+        c1 = '한';
+        System.out.println(c1);
+
+        c1 = 'C';
+        System.out.printf("%c : %d \n",c1,(int)c1);
+    }
+}
