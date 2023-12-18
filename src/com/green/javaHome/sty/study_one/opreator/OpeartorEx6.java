7cabc4f9e9ccec10f0c5136d8e53f51a4681b499
zkti1 <zkti12345@naver.com>
2023-12-18 10:48:19 +0900

    공부 12/18

---
+++ src/com/green/javaHome/sty/study_one/opreator/OpeartorEx6.java
@@ -0,0 +1,14 @@
+package com.green.javaHome.sty.study_one.opreator;
+
+public class OpeartorEx6 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        int result = num % 2;
+        System.out.printf("%d %%2 = %d\n", num, result);
+
+        num = 9;
+        result = num % 2;
+        System.out.printf("%d %%2 = %d\n", num, result);
+    }
+}
