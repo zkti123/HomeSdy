54d88b788417bb139bdc1ae17831317d97af0299
zkti1 <zkti12345@naver.com>
2023-12-18 10:56:24 +0900

    공부 12/18

---
+++ src/com/green/javaHome/sty/study_one/flow/FlowEx1.java
@@ -0,0 +1,13 @@
+package com.green.javaHome.sty.study_one.flow;
+
+public class FlowEx1 {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.printf("x=%d 일때, 참인 것은%n", x);
+
+        if(x==0) {System.out.println("x==0");}
+        if(x!=0) {System.out.println("x!=0");}
+        if(!(x==0)) {System.out.println("!(x==0)");}
+        if(!(x!=0)) {System.out.println("!(x!=0)");}
+    }
+}
