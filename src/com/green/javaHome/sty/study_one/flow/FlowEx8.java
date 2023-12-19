480068ee984881294dcad13cdfefa91705bb5877
zkti1 <zkti12345@naver.com>
2023-12-19 13:15:22 +0900

    공부 12/19

---
+++ src/com/green/javaHome/sty/study_one/flow/FlowEx8.java
@@ -0,0 +1,13 @@
+package com.green.javaHome.sty.study_one.flow;
+
+public class FlowEx8 {
+    public static void main(String[] args) {
+        int num=4;
+        for (int i = 1; i<num; i++){
+            for (int j = 1; j<num; j++){
+                System.out.printf("[%d,%d]",i ,j);
+            } System.out.println();
+        }
+
+    }
+}
