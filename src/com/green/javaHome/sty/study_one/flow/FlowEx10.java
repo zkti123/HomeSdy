af8ed5c63a36d5ba2174823042c31090843b34bc
zkti1 <zkti12345@naver.com>
2023-12-19 13:40:03 +0900

    공부 12/19

---
+++ src/com/green/javaHome/sty/study_one/flow/FlowEx10.java
@@ -0,0 +1,14 @@
+package com.green.javaHome.sty.study_one.flow;
+
+public class FlowEx10 {
+    public static void main(String[] args) {
+
+        int i = 5;
+
+        while (i>0){
+            --i;
+            System.out.printf(i +"- i can do it\n" );
+
+        }
+    }
+}
