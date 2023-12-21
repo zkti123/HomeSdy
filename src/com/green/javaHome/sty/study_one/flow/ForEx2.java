d7fb0e8b005e70bab39570d5b0f4605656118f97
zkti1 <zkti12345@naver.com>
2023-12-21 11:29:42 +0900

    공부 12/21

---
+++ src/com/green/javaHome/sty/study_one/flow/ForEx2.java
@@ -0,0 +1,21 @@
+package com.green.javaHome.sty.study_one.flow;
+
+public class ForEx2 {
+    public static void main(String[] args) {
+        //for( 초기화  ; 조건식  ; 증감식 ) 실행순서 : 1>2>S>3>2>s>3
+        for (int a = 21; a >= 18; a--) {
+            System.out.println("a");
+
+        }
+        for (int i =0; i<5; i++){
+            System.out.println(i+1);
+        }
+
+
+        for (int i =5; i<10; i++){
+            System.out.print(i-4);
+            System.out.print(", ");
+
+        }
+    }
+}
---
+++ src/com/green/javaHome/sty/study_one/flow/ForEx3.java
@@ -0,0 +1,26 @@
+package com.green.javaHome.sty.study_one.flow;
+
+public class ForEx3 {
+    public static void main(String[] args) {
+
+        //for( 초기화  ; 조건식  ; 증감식 ) 실행순서 : 1>2>S>3>2>s>3
+        //1,2,3,4,5
+
+
+        for (int i = 1; i<6; i++){
+            if (i <5){
+                System.out.print(i);
+            } else {
+                System.out.print("," );
+            }
+        }
+        System.out.println("/n---------------");
+
+        for (int i = 1; i<6; i++){
+            if (i>1){
+                System.out.print(",");
+            }
+            System.out.print(i);
+        }
+    }
+}
