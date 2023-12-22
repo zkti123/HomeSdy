6c6a420d7599ee7901a9492c8f884709c8711615
zkti1 <zkti12345@naver.com>
2023-12-22 10:25:57 +0900

    공부 12/22

---
+++ src/com/green/javaHome/sty/study_one/flow/SwitchEx1.java
@@ -0,0 +1,27 @@
+package com.green.javaHome.sty.study_one.flow;
+
+public class SwitchEx1 {
+    public static void main(String[] args) {
+        int level = 5;
+        switch(level){
+            case 5 :
+                System.out.println("level은 5");
+                break;
+            case 4 :
+                System.out.println("level은 4");
+                break;
+            case 3 :
+                System.out.println("level은 3");
+                break;
+            case 2 :
+                System.out.println("level은 2");
+                break;
+            case 1 :
+                System.out.println("level은 1");
+                break;
+            default:
+                System.out.println("level은 1~5가 아님");
+                //문자열 비교할때 swich가 편함
+        }
+    }
+}
