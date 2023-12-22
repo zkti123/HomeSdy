0b30b4603cb90c0c55622b176bddcbff4fdaacc4
zkti1 <zkti12345@naver.com>
2023-12-22 10:23:36 +0900

    공부 12/22

---
+++ src/com/green/javaHome/sty/study_one/flow/ForEx4.java
@@ -0,0 +1,13 @@
+package com.green.javaHome.sty.study_one.flow;
+
+public class ForEx4 {
+    public static void main(String[] args) {
+        for (int i=0; i<3; i++){
+
+            for (int z=0; z<5; z++){
+                System.out.printf("i:%d, z: %d\n", i, z);
+            }
+        }
+
+    }
+}
