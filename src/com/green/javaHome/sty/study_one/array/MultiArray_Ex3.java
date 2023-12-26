8bdce7968b9afe440b86dd599ace1ee61219fe54
zkti1 <zkti12345@naver.com>
2023-12-26 10:22:48 +0900

    공부 12/26

---
+++ src/com/green/javaHome/sty/study_one/array/MultiArray_Ex3.java
@@ -0,0 +1,11 @@
+package com.green.javaHome.sty.study_one.array;
+
+public class MultiArray_Ex3 {
+    public static void main(String[] args) {
+        int [][][] mArr = new int[4] [3][2];
+
+        mArr[0][0][0] = 10;
+        mArr[3][2][1] = 100;
+
+    }
+}
